cc1b66e7b66c9ec1356c056d718767534971397c
Paul <paul@gobble.local>
2025-12-22 16:34:20 +0100

    update

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Stopping old server..."
+echo "Stopping old servers..."
 pkill -f "node index.js" || true
+pkill -f "serve -s dist" || true
 sleep 1
 
-echo "Starting server..."
-cd /home/freebox/gobble/server || exit 1
+echo "Starting backend..."
+cd /home/freebox/gobble_git/server || exit 1
 NODE_ENV=production nohup node index.js > ../server.log 2>&1 &
 
-echo "Server started"
+echo "Done."
